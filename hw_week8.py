0ac392ee047e38ca6121944bea6148909d9e8760
AllenLyu <a314007@gmail.com>
2020-03-09 13:33:50 +0800

    update_hw_week8

--- hw_week8.py
+++ hw_week8.py
@@ -11,7 +11,7 @@ def hw_week8():
      str(win) + ' Wins, '+ str(losses) +' Losses, ' +str(ties)+' Ties '
     )
 
-    print('Enter your move:(r)ock (p)aper (s)cissors or (q)uit')
+    print('Enter your move: (r)ock (p)aper (s)cissors or (q)uit')
 
     user_input = input()
     if user_input =='r':
@@ -37,19 +37,19 @@ def hw_week8():
       print ('It is a tie!')
       ties=ties+1
     elif user_input == 'r' and computer_chose =='p':
-      print ('You loss!')
+      print ('You lose!')
       losses=losses+1
     elif user_input == 'r' and computer_chose =='s':
       print ('You win!')
       win=win+1
     elif user_input == 'p' and computer_chose =='s':
-      print ('You loss!')
+      print ('You lose!')
       losses=losses+1
     elif user_input == 'p' and computer_chose =='r':
       print ('You win!')
       win=win+1
     elif user_input == 's' and computer_chose =='r':
-      print ('You loss!')
+      print ('You lose!')
       losses=losses+1
     elif user_input == 's' and computer_chose =='p':
       print ('You win!')
